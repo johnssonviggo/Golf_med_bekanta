9c053b10be0765462a921ed553e500ca2333ddd8
johnssonviggo <viggo.johnsson1@gmail.com>
2024-03-14 14:58:31 +0100

    startade med javascript projekt

---
+++ golf.rb
@@ -0,0 +1,19 @@
+require 'ruby2d'
+
+set title: 'Golf Med Vänner'
+set width: 1024
+set height: 768
+
+
+circle = Circle.new(x: 300, y: 200, radius: rand(1..20), color: 'white')
+
+class Player
+end
+
+class Obstacle
+end
+
+class Goal
+end
+
+show
---
+++ sketch.js
@@ -0,0 +1,16 @@
+class Player {
+    constructor() {
+        this.x = 0;
+        this.y = 0;
+    }
+}
+
+let player = new Player();
+
+function setup() {
+    createCanvas(400, 400);
+}
+
+function draw() {
+    background(0);
+}
