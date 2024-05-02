4692704aafbd1055ce25137393ed0ae97b5133af
johnssonviggo <viggo.johnsson1@gmail.com>
2024-05-02 15:40:54 +0200

    man kan vinna nu

--- golf.rb
+++ golf.rb
@@ -6,17 +6,22 @@ set height: 768
 set background: 'green'
 
 class Ball
-  attr_accessor :x , :y, :velocity, :angle
-  HEIGHT = 25
-  def initialize(x_init, y_init)
+  attr_accessor :x , :y, :velocity, :angle, :radius
+
+  def initialize(x_init, y_init, radius)
     @x = x_init
     @y = y_init
     @xv = 0
     @yv = 0
     @velocity = 0
     @angle = 0
-    @shape = Circle.new(x: @x, y: @y,
-    radius: HEIGHT, color: 'white')
+    @radius = radius
+    @shape = Circle.new(
+      x: @x,
+      y: @y,
+      radius: radius,
+      color: 'white'
+    )
   end
 
   def shoot
@@ -30,21 +35,33 @@ class Ball
 
     @shape.x = @x
     @shape.y = @y
-    @xv *= 0.97
-    @yv *= 0.97
+    @xv *= 0.95
+    @yv *= 0.95
 
 
     if hit_bottom? || hit_top?
       @yv = -@yv
     end
+    if hit_left? || hit_right?
+      @xv = -@xv
+    end
+
   end
 
   def hit_bottom?
-    @y + HEIGHT >= Window.height
+    @y + @radius >= Window.height
   end
 
   def hit_top?
     @y <= 0
+   end
+
+   def hit_left?
+    @x <= 0
+   end
+
+   def hit_right?
+    @x + @radius >= Window.width
    end
 end
 
@@ -58,16 +75,18 @@ class Obstacle
 end
 
 class Hole
-  attr_accessor :x , :y
-  def initialize(x_init, y_init)
+  attr_accessor :x , :y, :radius
+
+  def initialize(x_init, y_init, radius)
     @x = x_init
     @y = y_init
-    @goal = Circle.new(x: 700, y: rand(200..700), radius: 40, color: 'black')
+    @radius = radius
+    @goal = Circle.new(x: @x, y: @y, radius: @radius, color: 'black')
   end
 end
 
-golfball = Ball.new(300,300)
-hole = Hole.new(400,400)
+golfball = Ball.new(rand(100..300),rand(100..600),25)
+hole = Hole.new(rand(400..700),rand(200..700), 40)
 rectangle = Obstacle.new(500, 200)
 
 xd = 0
@@ -87,15 +106,23 @@ on :mouse_up do |event|
   xu = event.x
   yu = event.y
 
-  golfball.velocity = Math.sqrt((xu-xd)**2 + (yu-yd)**2) * 0.25
+  golfball.velocity = Math.sqrt((xu-xd)**2 + (yu-yd)**2) * 0.1
   golfball.angle = Math.atan2(yu-yd, xu-xd) + Math::PI
 
   golfball.shoot
 end
 
-
 update do
   golfball.move
+
+  if hole.radius > Math.sqrt((hole.x - golfball.x)**2 + (hole.y - golfball.y)**2)
+    you_win = Text.new(
+      'YOU WIN',
+      y: 100,
+      style: 'bold',
+      size: 70)
+    you_win.x = (Window.width - you_win.width)/2
+    end
 end
 
 
